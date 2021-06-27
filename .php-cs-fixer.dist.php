2a48cb95f7d6e8adfc2f5045fc6335c6cb6e8eb5
Natan Felles <natanfelles@gmail.com>
2021-06-27 17:37:12 -0300

    Coding Standard: Add header comments

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of The Framework Language Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 use Framework\CodingStandard\Config;
 use Framework\CodingStandard\Finder;
 
@@ -7,4 +14,4 @@ return (new Config())->setFinder(
 	Finder::create()->in(__DIR__)
 )->replaceRules([
 	'non_printable_character' => false,
-]);
+])->setDefaultHeaderComment('The Framework Language Library');
--- tests/locales-1/pt/tests.php
+++ tests/locales-1/pt/tests.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of The Framework Language Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 return [
 	'hello' => 'Olá, {0}!',
 ];
