feec00a4897d61080e8410239c65943caf0703bf
Natan Felles <natanfelles@gmail.com>
2021-07-14 11:04:15 -0300

    Update default header comments

--- src/Language.php
+++ src/Language.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /*
- * This file is part of The Framework Language Library.
+ * This file is part of Aplus Framework Language Library.
  *
  * (c) Natan Felles <natanfelles@gmail.com>
  *
